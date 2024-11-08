bb6084211384fd410bdec28d9e36f6e359b66a37
Jack12404 <jackli@computingforall.org>
2024-11-07 17:20:36 -0800

    view sets cards from db work

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const cors = require('cors');
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
--- Client/animations.js
+++ Client/animations.js
@@ -1,11 +1,11 @@
 let scrollContainer = document.querySelector("#setList");
 let backBtn = document.getElementById("back-btn");
 let nextBtn = document.getElementById("next-btn");
-const card = document.querySelector('.card__inner');
+// const card = document.querySelector('.card__inner');
 
-card.addEventListener('click', ()=> {
-    card.classList.toggle('is-flipped');
-});
+// card.addEventListener('click', ()=> {
+//     card.classList.toggle('is-flipped');
+// });
 
 scrollContainer.addEventListener('wheel', (e) => {
     e.defaultPrevented();
