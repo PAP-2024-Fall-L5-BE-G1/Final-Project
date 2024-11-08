9462c4bb99f0c3fddb5dcc05d9e981d3ff0b2c7a
Dishita Soni <dishitasoni@dismac.local>
2024-11-07 17:49:21 -0800

    added flashcard scroll

--- Client/animations.js
+++ Client/animations.js
@@ -1,18 +1,12 @@
 let scrollContainer = document.querySelector("#setList");
-let backBtn = document.getElementById("back-btn");
-let nextBtn = document.getElementById("next-btn");
-const card = document.querySelector('.card__inner');
-
-card.addEventListener('click', ()=> {
-    card.classList.toggle('is-flipped');
-});
+let backBtn = document.querySelector(".back-btn");
+let nextBtn = document.querySelector(".next-btn");
 
 scrollContainer.addEventListener('wheel', (e) => {
     e.defaultPrevented();
     scrollContainer.scrollLeft += e.deltaY;
     scrollContainer.style.scrollBehavior = 'auto';
 });
-
 nextBtn.addEventListener("click", () => {
     scrollContainer.style.scrollBehavior = 'smooth';
     scrollContainer.scrollLeft += 900;
